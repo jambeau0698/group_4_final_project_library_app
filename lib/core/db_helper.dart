4d4dc73f24d334872f5e218944b25220adeb553b
grannystillgotit <73319672+grannystillgotit@users.noreply.github.com>
2025-04-16 15:25:21 -0400

    db helper is initalized with create table method

--- lib/core/db_helper.dart
+++ lib/core/db_helper.dart
@@ -11,19 +11,60 @@ class DBHelper {
   DBHelper._init();
   
   Future<Database> get libraryDatabase async {
-    if (_database !=) return _database;
+    if (_database != null) return _database!;
     _database = await _getDB();
-    return _database;
+    return _database!;
   }
 
   Future<Database> _getDB() async{
     final dbPath = await getDatabasesPath();
-    final path = join(dbPath, '')
+    final path = join(dbPath, 'Final.db');
+
+    return await openDatabase(
+        path,
+      version: 1,
+        onCreate: _createDatabase,
+    );
   }
 
 
 // create data base class 3 tables student table book table linking table include the witrhdrawn date an return date into linking table
+Future<void> _createDatabase(Database db, int version) async{
+    await db.execute('''
+    CREATE TABLE student (
+    studentId INTERGER PRIMARY KEY AUTOINCREMENT,
+    studentNumber TEXT,
+    fullName TEXT,
+    email TEXT,
+    password TEXT,
+    year TEXT,
+    program TEXT,
+      )
+      ''');
 
+    await db.execute('''
+    CREATE TABLE books (
+    bookID INTERGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    genre TEXT,
+    description TEXT,
+    bookCover TEXT,
+    author TEXT,
+    withdrawn BOOL DEFAULT false   
+    )
+    ''');
+
+    await db.execute(
+      '''
+      CREATE TABLE withdrawn (
+      withdrawId INTERGER PRIMARY KEY AUTOINCREMENT,
+      studentId INTEGER FOREIGN KEY,
+      bookId INTEGER FOREIGN KEY
+      withdrawDate DATETIME,
+      dueDate DATETIME
+      )
+   ''' );
+}
 
 // read all books function
 
