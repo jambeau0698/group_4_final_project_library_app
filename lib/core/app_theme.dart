62ed5977f3c85bb3b09af3989c73f761b601cbc3
cjgioia21 <bosscjgioia@gmail.com>
2025-04-17 13:58:09 -0400

    basic colors and started on app theme

--- lib/core/app_theme.dart
+++ lib/core/app_theme.dart
@@ -2,5 +2,22 @@ import 'package:flutter/material.dart';
 import 'app_colors.dart';
 
 class AppTheme {
-
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primaryColor: AppColors.primary,
+      scaffoldBackgroundColor: AppColors.background,
+      textTheme: const TextTheme(
+        bodyMedium: TextStyle(color: AppColors.text),
+      ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12)
+        )
+      )
+    )
+    );
+  }
 }
