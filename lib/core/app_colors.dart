004af3e2ab595bcda959500eb7d9eea27d5eeb2d
cjgioia21 <bosscjgioia@gmail.com>
2025-04-17 11:01:43 -0400

    basic colors and started on app theme

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Colors.blue;
+  static const secondary = Colors.grey;
+  static const background = Colors.white;
+  static const text = Colors.black;
+  static const error = Colors.red;
+}
---
+++ lib/core/app_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppTheme {
+
+}
