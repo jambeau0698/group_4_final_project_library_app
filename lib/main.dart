fcf7c92bdb2f710f418c0e6acbbc8f55b9392644
cjgioia21 <bosscjgioia@gmail.com>
2025-04-17 20:50:03 -0400

    fixed main

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:group_4_final_project_library_app/views/splash_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
